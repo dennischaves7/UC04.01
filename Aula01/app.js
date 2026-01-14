f18124480744968c402c941b1a9ead4a0cff1055
Dennis Barbosa <dennisbarbosa998@gmail.com>
2026-01-13 21:29:43 -0300

    feat: API inicio 2

--- Aula01/app.js
+++ Aula01/app.js
@@ -6,3 +6,9 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+export default app;
