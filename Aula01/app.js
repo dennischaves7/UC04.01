e9ca8b6f07a6192baa8fe9e2ee394bd32bd40488
dennischaves7 <dennisbarbosa998@gmail.com>
2026-01-14 21:30:55 -0300

    feat: API 02

--- Aula01/app.js
+++ Aula01/app.js
@@ -4,11 +4,24 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send({ message: 'Hello World! This is my first Express app.' });
 });
+
+app.get('/teste', (req, res) => {
+  res.json({
+    nome: "Dennis",
+    religiao: 'Corinthians',
+    cidade: "Corinthians"
+  });
+});
+
+app.get('/status', (req, res) => {
+  res.status(200).send('Status OK');
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
-});
+})
 
 export default app;
